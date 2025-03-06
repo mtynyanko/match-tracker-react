f15acb032fe336d8e77641d5a798e219319a4498
Maxim Tynyanko <data.expanged@gmail.com>
2025-03-06 18:24:23 +0300

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
+import refreshIcon from "../../assets/icons/Refresh.svg"
 import "./Header.css"
 
 const Header = () => {
   return (
     <div className="header">
       <p className="title">Match Tracker</p>
-      <button className="refresh-button">Oбновить</button>
+      <button className="refresh-button">
+        <p className="button-text">Обновить</p>
+        <img src={refreshIcon} alt="refresh-icon" className="button-icon" />
+      </button>
     </div>
   );
 };
